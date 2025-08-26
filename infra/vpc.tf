8c3a083081cd0c775ebf2285f44aac10eeb38b69
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-08-26 03:58:10 +0300

    feature: add vpc

---
+++ infra/vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                          = var.vpc_name
+  cidr                          = var.vpc_cidr
+  azs                           = var.azs
+  private_subnets               = var.private_subnets
+  public_subnets                = var.public_subnets
+  enable_nat_gateway            = var.enable_nat_gateway
+  single_nat_gateway            = true
+  one_nat_gateway_per_az        = false
+  enable_vpn_gateway            = var.enable_vpn_gateway
+  manage_default_security_group = var.manage_default_security_group
+  tags                          = var.tags
+}
