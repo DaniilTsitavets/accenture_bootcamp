eb8cec09968993a7dec182c9bfbd53a38e7ad96a
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-08-26 13:14:15 +0300

    feature: add s3 backend comment

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region  = var.region
   profile = "dev-mfa"
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket = ""
+#     use_lockfile = true
+#     key    = "infra/"
+#     region = "eu-west-1"
+#   }
+# }
