85c2c3458a77471744ba1ea23e3417fa75117418
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-08-27 14:14:55 +0300

    fix: provide right config for ecs dashboard

--- infra/cloudwatch-dashboard-ecs.tf
+++ infra/cloudwatch-dashboard-ecs.tf
@@ -1,10 +1,7 @@
-
 # ECS Dashboard
 module "ecs_dashboard" {
-  source  = "HENNGE/cloudwatch-dashboard/aws"
- 
-  name = "ECS-Tasks-Dashboard"
-
+  source = "HENNGE/cloudwatch-dashboard/aws"
+  name   = "ECS-Tasks-Dashboard"
   widgets = [
     module.ecs_cpu.widget_object,
     module.ecs_memory.widget_object,
@@ -15,57 +12,76 @@ module "ecs_dashboard" {
 
 # CPU Utilization widget
 module "ecs_cpu" {
-  source  = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  source = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  region = var.region
+  title  = "ECS CPU Utilization"
+  width  = 12
+  period = 60
 
-  namespace  = "AWS/ECS"
-  metric     = "CPUUtilization"
-dimensions = [
-    { Name = "ClusterName", Value = module.ecs-cluster.name },
-    { Name = "ServiceName", Value = module.ecs-service.name }
+  metrics = [
+    [
+      "AWS/ECS",
+      "CPUUtilization",
+      "ClusterName", module.ecs-cluster.name,
+      "ServiceName", module.ecs-service.name,
+      { stat = "Average" }
+    ]
   ]
-  title      = "ECS CPU Utilization"
-  stat       = "Average"
 }
 
 # Memory Utilization widget
 module "ecs_memory" {
-  source  = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  source = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  region = var.region
+  title  = "ECS Memory Utilization"
+  width  = 12
+  period = 60
 
-  namespace  = "AWS/ECS"
-  metric     = "MemoryUtilization"
-  dimensions = [
-    { Name = "ClusterName", Value = module.ecs-cluster.name },
-    { Name = "ServiceName", Value = module.ecs-service.name }
+  metrics = [
+    [
+      "AWS/ECS",
+      "MemoryUtilization",
+      "ClusterName", module.ecs-cluster.name,
+      "ServiceName", module.ecs-service.name,
+      { stat = "Average" }
+    ]
   ]
-  title      = "ECS Memory Utilization"
-  stat       = "Average"
 }
 
 # Running Task Count widget
 module "ecs_running_tasks" {
-  source  = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  source = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  region = var.region
+  title  = "ECS Running Task Count"
+  width  = 12
+  period = 60
 
-  namespace  = "AWS/ECS"
-  metric     = "RunningTaskCount"
-  dimensions = [
-    { Name = "ClusterName", Value = module.ecs-cluster.name },
-    { Name = "ServiceName", Value = module.ecs-service.name }
+  metrics = [
+    [
+      "ECS/ContainerInsights",
+      "RunningTaskCount",
+      "ClusterName", module.ecs-cluster.name,
+      "ServiceName", module.ecs-service.name,
+      { stat = "Maximum" }
+    ]
   ]
-  title      = "ECS Running Task Count"
-  stat       = "Maximum"
 }
 
 # Desired Task Count widget
 module "ecs_desired_tasks" {
-  source  = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  source = "HENNGE/cloudwatch-dashboard/aws//modules/widget/metric"
+  region = var.region
+  title  = "ECS Desired Task Count"
+  width  = 12
+  period = 60
 
-
-  namespace  = "AWS/ECS"
-  metric     = "DesiredTaskCount"
-  dimensions = [
-    { Name = "ClusterName", Value = module.ecs-cluster.name },
-    { Name = "ServiceName", Value = module.ecs-service.name }
+  metrics = [
+    [
+      "ECS/ContainerInsights",
+      "DesiredTaskCount",
+      "ClusterName", module.ecs-cluster.name,
+      "ServiceName", module.ecs-service.name,
+      { stat = "Maximum" }
+    ]
   ]
-  title      = "ECS Desired Task Count"
-  stat       = "Maximum"
 }
