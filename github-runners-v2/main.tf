425e7bcab415f9e088fa20e30b3376737da1bd0b
Viktorija Miftahova <viktorija.miftahova@gmail.com>
2025-08-28 16:02:09 +0300

    remove profile bind in provider block

--- github-runners-v2/main.tf
+++ github-runners-v2/main.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "team2-backend-2025-aug"
     key            = "github-runners-v2.tfstate"
     region         = "eu-west-1"
-    profile        = "dev-mfa"
   }
 }
 
